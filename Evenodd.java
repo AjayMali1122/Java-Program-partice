66c99fa0036b4e305c3cb6167d77de9c9cb4ac02
Ajay Mali <168232654+AjayMali1122@users.noreply.github.com>
2024-07-31 21:19:29 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,14 @@
+ class Evenodd
+  {
+    public static void main(String[] args) {
+        int no = 99;
+        if (no % 2 == 0)
+        {
+            System.out.println("no is even");
+        } else {
+            System.out.println("no is odd");
+        }
+
+    }
+
+}
